62edb762745b5ec03425378a3077be1a0baf3b03
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-06-21 15:53:07 -0300

    Simplifying to not be an overkill.

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	r := gin.Default()
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(os.Getenv("APP_PORT"))
+}
