1637de7ec9b6ba8d2c54b92882cb0d9fda9c0b0d
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-07-06 14:45:50 -0300

    Create new modules
    * controllers
    * server

--- main.go
+++ main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-)
+import "github.com/abispo/financial-control-backend/server"
 
 func main() {
-	r := gin.Default()
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	// db.Init()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(os.Getenv("APP_PORT"))
+	server.Init()
 }
---
+++ server/router.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"github.com/abispo/financial-control-backend/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	health := new(controllers.HealthController)
+
+	router.GET("/health", health.Status)
+	return router
+}
