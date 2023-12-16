67321cdf031f9ebb21ebc7ce68cd0bd7d965d8f7
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-12-16 19:34:03 -0300

    healthcheck endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,16 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.get("/healthcheck", (req: Request, res: Response) => {
+    res.send({
+        uptime: process.uptime(),
+        message: "OK",
+        timestamp: new Date().toISOString().slice(0, -5) + 'Z'
+    })
+})
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Ok")
+    res.send("OK")
 });
 
 app.listen(port, () => {
